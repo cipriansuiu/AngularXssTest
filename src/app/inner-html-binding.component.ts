ad5501fb4eb2691202ccf07563a39a9cf9d5e0e0
cipriansuiu <suiuciprianconstantin@gmail.com>
2019-11-14 13:30:52 +0000

    first commit

---
+++ src/app/inner-html-binding.component.ts
@@ -0,0 +1,5 @@
+export class InnerHtmlBindingComponent {
+    // For example, a user/attacker-controlled value from a URL.
+    htmlSnippet = 'Template <script>alert("0wned")</script> <b>Syntax</b>';
+    
+  }
